9f2df7c623c514ebbd974a229856a0d9dfbf2b74
Jack McClure <jackmccl@meta.com>
2025-06-26 22:48:07 -0700

    Fixed challenge code extraction from params to query

--- server/routes/indexRoute.js
+++ server/routes/indexRoute.js
@@ -40,8 +40,7 @@ app.get('/ebay', (req, res) => {
     try {
         const crypto = require('crypto')
         console.log("Ebay get endpoint: ")
-        const { challenge_code } = req.params
-        console.log(req.params)
+        const { challenge_code } = req.query
         const challengeCode = challenge_code
         const verificationToken = "879878121345134523452345234523452354"
         const endpoint = "https://kudos-board-backend-8gm7.onrender.com/ebay"
